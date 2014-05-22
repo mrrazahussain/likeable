bc4d3ba9c304693dccc80b4587266d9b062e8385
Adnan Tahir <mr.raza.hussain@gmail.com>
2014-05-23 00:57:10 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likeable::Application.config.secret_token = '3d8451099789106a6ac81f036aa45f59acb7a0b11a0fdcdf3db116f17fd591705ce7b8c3ebeee52c10a647adfea18b637f4d4c0d9e14c1aa184bbc3ff30b57a4'
